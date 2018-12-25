47586772c7aa4cdc0f1c5790044a3216c24c7965
Bruno Tacio <bruno018@gmail.com>
2018-12-25 16:19:44 -0200

    initial commit

---
+++ server.js
@@ -0,0 +1,57 @@
+const express = require('express')
+const ejs = require('ejs')
+const app = express()
+const path = require('path')
+const fs = require('fs')
+
+app.set('view engine', 'ejs')
+app.set('partials', path.join( __dirname + '/views/partials'))
+
+app.use((req, res, next) => {
+  const now = new Date().toString()
+  const log = `${now}: ${req.method} ${req.url}`
+
+  console.log(log)
+  fs.appendFile('server,log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log')
+    }
+  })
+  next()
+})
+
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.ejs', {
+//     pageTitle: 'We\'ll be right back',
+//     MaintenanceMsg: 'This site is currently being updated'
+//   })
+// })
+
+app.use(express.static(__dirname + '/public'))
+
+app.get('/', (req, res) => {
+  // res.send('<h1>Hello Expess</h1>')
+  res.render('home.ejs', {
+    pageTitle : 'Some Nice Website',
+    welcomeMsg: 'Be welcome my friend!'
+  })
+})
+
+
+app.get('/about', (req, res) => {
+  res.render('about.ejs', {
+    pageTitle: 'About Page'
+  })
+})
+
+
+app.get('/bad', (req, res) => {
+  res.send({
+    errorMessage: 'Unable to fullfill this request'
+  })
+})
+
+app.listen(3000, () => {
+  console.log('Server running on port: 3000')
+})
